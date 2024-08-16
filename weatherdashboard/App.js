f4cdbac69994873f8dcdf826c137ccccfa2b59d1
VAISHNAVI KOLASANI <155756613+Vaishnavik05@users.noreply.github.com>
2024-08-16 16:45:29 +0530

    Create App.js

---
+++ weatherdashboard/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+import Weather from './Components.jsx/Weather';
+function App() {
+  return (
+    
+    <Weather/>
+    
+  );
+}
+export default App;
